ec4a5bf50053c454e97a48609c0377bea9e91a55
lesleymaraina <lesley.maraina@gmail.com>
2017-04-11 12:42:29 -0400

    Updates

--- Python/Pipeline/Rand_Selection_VCF/Rand_Selection_VCF.py
+++ Python/Pipeline/Rand_Selection_VCF/Rand_Selection_VCF.py
@@ -22,7 +22,7 @@ import numpy as np
 
 # Load/Parse .txt file using pd.read_table()
 colnames = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-df = pd.read_table('/Users/lmc2/Desktop/NIST/Projects/ML1-TrainingData/Code/union_refalt.sort.Jan31.3.txt', names=colnames)
+df = pd.read_table('/Volumes/lesleydata/SVVIZOutput/April112017/Step1/RandomSelection.VCFs/union_refalt.sort.formattedforsvviz.txt', names=colnames)
 print df.head(5)
 # print len(df)
 
@@ -50,24 +50,40 @@ df['VariantType'].fillna('', inplace=True)
 dfDel = df[df['VariantType'].str.contains("DEL")]
 dfIns = df[df['VariantType'].str.contains("INS")]
 
+'''
+Deletions
+'''
 # Take a random sample of 5000 INS and 5000 DEL and store them in respective .tsv files
 row = random.sample(dfDel.index, 5000)
 df_del = dfDel.ix[row]
-df_del_1to1000 = df_del[:1000]
-df_del_1001to2000 = df_del[1001:2000]
-df_del_2001to3000 = df_del[2001:3000]
-df_del_3001to4000 = df_del[3001:4000]
-df_del_4001to5000 = df_del[4001:5000]
+# print ('These are the deletions', df_del.head())
+# print df_del.shape
+
+
+df_del_1to1000 = df_del.iloc[:1000]
+df_del_1001to2000 = df_del.iloc[1001:2000]
+df_del_2001to3000 = df_del.iloc[2001:3000]
+df_del_3001to4000 = df_del.iloc[3001:4000]
+df_del_4001to5000 = df_del.iloc[4001:5000]
 # print df_del.head(5)
 # print len(df_del)
-df_del.to_csv('/Users/lmc2/Desktop/NIST/Projects/ML1-TrainingData/Code/PythonCode/Feb102017/SelectINSDEL.size/Output/DEL/union_refalt.sort.DEL.1to1000.tsv', sep='\t', header=None, index=False)
-df_del.to_csv('/Users/lmc2/Desktop/NIST/Projects/ML1-TrainingData/Code/PythonCode/Feb102017/SelectINSDEL.size/Output/DEL/union_refalt.sort.DEL.1001to2000.tsv', sep='\t', header=None, index=False)
-df_del.to_csv('/Users/lmc2/Desktop/NIST/Projects/ML1-TrainingData/Code/PythonCode/Feb102017/SelectINSDEL.size/Output/DEL/union_refalt.sort.DEL.2001to3000.tsv', sep='\t', header=None, index=False)
-df_del.to_csv('/Users/lmc2/Desktop/NIST/Projects/ML1-TrainingData/Code/PythonCode/Feb102017/SelectINSDEL.size/Output/DEL/union_refalt.sort.DEL.3001to4000.tsv', sep='\t', header=None, index=False)
-df_del.to_csv('/Users/lmc2/Desktop/NIST/Projects/ML1-TrainingData/Code/PythonCode/Feb102017/SelectINSDEL.size/Output/DEL/union_refalt.sort.DEL.4001to5000.tsv', sep='\t', header=None, index=False)
 
+df_del_1to1000.to_csv('/Volumes/lesleydata/SVVIZOutput/April112017/Step1/RandomSelection.VCFs/VCF/DEL/1-1000/union_refalt.sort.DEL.1to1000.tsv', sep='\t', header=None, index=False)
+df_del_1001to2000.to_csv('/Volumes/lesleydata/SVVIZOutput/April112017/Step1/RandomSelection.VCFs/VCF/DEL/1001-2000/union_refalt.sort.DEL.1001to2000.tsv', sep='\t', header=None, index=False)
+df_del_2001to3000.to_csv('/Volumes/lesleydata/SVVIZOutput/April112017/Step1/RandomSelection.VCFs/VCF/DEL/2001-3000/union_refalt.sort.DEL.2001to3000.tsv', sep='\t', header=None, index=False)
+df_del_3001to4000.to_csv('/Volumes/lesleydata/SVVIZOutput/April112017/Step1/RandomSelection.VCFs/VCF/DEL/3001-4000/union_refalt.sort.DEL.3001to4000.tsv', sep='\t', header=None, index=False)
+df_del_4001to5000.to_csv('/Volumes/lesleydata/SVVIZOutput/April112017/Step1/RandomSelection.VCFs/VCF/DEL/4001-5000/union_refalt.sort.DEL.4001to5000.tsv', sep='\t', header=None, index=False)
+
+
+'''
+Insertions
+'''
 row = random.sample(dfIns.index, 5000)
 df_ins = dfIns.ix[row]
+# print ('These are the insertions', df_ins.head())
+# print df_del.shape
+
+
 df_ins_1to1000 = df_ins[:1000]
 df_ins_1001to2000 = df_ins[1001:2000]
 df_ins_2001to3000 = df_ins[2001:3000]
@@ -75,12 +91,12 @@ df_ins_3001to4000 = df_ins[3001:4000]
 df_ins_4001to5000 = df_ins[4001:5000]
 # print df_ins.head(5)
 # print len(df_ins)
-# df_ins.to_csv('/Users/lmc2/Desktop/NIST/Projects/ML1-TrainingData/Code/PythonCode/Feb102017/SelectINSDEL.size/Output/INS/union_refalt.sort.INS.tsv', sep='\t', header=None, index=False)
-df_ins.to_csv('/Users/lmc2/Desktop/NIST/Projects/ML1-TrainingData/Code/PythonCode/Feb102017/SelectINSDEL.size/Output/INS/union_refalt.sort.INS.1to1000.tsv', sep='\t', header=None, index=False)
-df_ins.to_csv('/Users/lmc2/Desktop/NIST/Projects/ML1-TrainingData/Code/PythonCode/Feb102017/SelectINSDEL.size/Output/INS/union_refalt.sort.INS.1001to2000.tsv', sep='\t', header=None, index=False)
-df_ins.to_csv('/Users/lmc2/Desktop/NIST/Projects/ML1-TrainingData/Code/PythonCode/Feb102017/SelectINSDEL.size/Output/INS/union_refalt.sort.INS.2001to3000.tsv', sep='\t', header=None, index=False)
-df_ins.to_csv('/Users/lmc2/Desktop/NIST/Projects/ML1-TrainingData/Code/PythonCode/Feb102017/SelectINSDEL.size/Output/INS/union_refalt.sort.INS.3001to4000.tsv', sep='\t', header=None, index=False)
-df_ins.to_csv('/Users/lmc2/Desktop/NIST/Projects/ML1-TrainingData/Code/PythonCode/Feb102017/SelectINSDEL.size/Output/INS/union_refalt.sort.INS.4001to5000.tsv', sep='\t', header=None, index=False)
+
+df_ins_1to1000.to_csv('/Volumes/lesleydata/SVVIZOutput/April112017/Step1/RandomSelection.VCFs/VCF/INS/1-1000/union_refalt.sort.INS.1to1000.tsv', sep='\t', header=None, index=False)
+df_ins_1001to2000.to_csv('/Volumes/lesleydata/SVVIZOutput/April112017/Step1/RandomSelection.VCFs/VCF/INS/1001-2000/union_refalt.sort.INS.1001to2000.tsv', sep='\t', header=None, index=False)
+df_ins_2001to3000.to_csv('/Volumes/lesleydata/SVVIZOutput/April112017/Step1/RandomSelection.VCFs/VCF/INS/2001-3000/union_refalt.sort.INS.2001to3000.tsv', sep='\t', header=None, index=False)
+df_ins_3001to4000.to_csv('/Volumes/lesleydata/SVVIZOutput/April112017/Step1/RandomSelection.VCFs/VCF/INS/3001-4000/union_refalt.sort.INS.3001to4000.tsv', sep='\t', header=None, index=False)
+df_ins_4001to5000.to_csv('/Volumes/lesleydata/SVVIZOutput/April112017/Step1/RandomSelection.VCFs/VCF/INS/4001-5000/union_refalt.sort.INS.4001to5000.tsv', sep='\t', header=None, index=False)
 
 # SOURCE: randomly select rows
 # http://stackoverflow.com/questions/12190874/pandas-sampling-a-dataframe
