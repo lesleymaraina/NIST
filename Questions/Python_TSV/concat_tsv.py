83fc9a9c857415aae40cb2796aa02f1b9cc67215
CONGO Faïçal Yannick Palingwendé <yannick.congo@gmail.com>
2017-11-21 11:01:48 -0500

    Looping over all the files in tsv_files and dumping the csv convertions into csv_files.

--- Questions/Python_TSV/concat_tsv.py
+++ Questions/Python_TSV/concat_tsv.py
@@ -1,13 +1,26 @@
 import pandas as pd
-colnames = ['variant',	'sample',	'allele',	'key',	'value']
-df = pd.read_table('/Volumes/LMC/tsv_files/summary_union_refalt.sort.DEL.1to1000_274.tsv')
-# pd.crosstab(index=df['values'], columns=[df['convert_me'], df['age_col']])
+import glob
 
-# print (df.head(1))
-df['combined'] = df['allele'].astype(str)+'_'+df['key'].astype(str)
-df2 = df.pivot_table(values='value', index=['variant', 'sample'], columns='combined')
+# All the code in a dump function.
+# It gets the path to a tsv file.
+# Then dumps a csv file in csv_files.
+# Filenames is the last word in the tsv name witout the extension.
+def dump_csv(path):
+    colnames = ['variant',    'sample',    'allele',    'key',    'value']
 
+    df = pd.read_table(path)
+    # pd.crosstab(index=df['values'], columns=[df['convert_me'], df['age_col']])
 
-print (df2.head(1))
+    # print (df.head(1))
+    df['combined'] = df['allele'].astype(str)+'_'+df['key'].astype(str)
+    df2 = df.pivot_table(values='value', index=['variant', 'sample'], columns='combined')
 
-df2.to_csv('df2.csv', index=True)
+    # print (df2.head(1))
+
+    df2.to_csv('csv_files/{0}.csv'.format(path.split("/")[-1].split(".")[-2]), index=True)
+
+if __name__ == '__main__':
+	# We loop over all the files .tsv and convert each.
+    for path in glob.glob("tsv_files/*.tsv"):
+        print("Converting {0}...".format(path))
+        dump_csv(path)
