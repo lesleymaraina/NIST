a237abc3d1ed5f1e1d0671b57ad4f3c6d50f08f7
lesleymaraina <lesley.maraina@gmail.com>
2018-02-12 16:20:02 -0500

    updates

--- Python/Flask_Apps/manCur/app.py
+++ Python/Flask_Apps/manCur/app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField, RadioField, SelectField
+from wtforms.validators import InputRequired
+from flask_boostrap import Bootstrap
 
 
 app = Flask(__name__)
@@ -8,13 +12,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/lmc2/manCur_Flask/man
 
 db = SQLAlchemy(app)
 
-@app.route('/')
+class MyForm(FlaskForm):
+	radios = RadioField('Radios', choices=[('Homozygous Reference', 'Homozygous Reference'),('Heterozygous Variant', 'Heterozygous Variant'),('Homozygous Variant','Homozygous Variant'),('Complex [ie: 2+ variants in this region] or difficult', 'Complex [ie: 2+ variants in this region] or difficult')])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
 	return render_template('index.html')
-
-# @app.route('/about')
-# def about():
-# 	return render_template('about.html')
 
 
 if __name__ == '__main__':
